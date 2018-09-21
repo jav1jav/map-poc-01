f0236b80651f5353636964d302f36089be9d8b42
Javier Carey <javier3@gmail.com>
2018-09-21 10:53:27 -0700

    Built a component that uses sockets to emit stats, updated client side socket.js to forward those stats to the server

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,17 @@
 import io from 'socket.io-client'
+import { broadcaster } from './components/broadcastStats'
 
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
+})
+
+//socket.on('sendRunnerStats', )
+
+broadcaster.on('sendRunnerStats', (...payload) => {
+  socket.emit('sendRunnerStats', ...payload);
+  console.log('client', ...payload);
 })
 
 export default socket
