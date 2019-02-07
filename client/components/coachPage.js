75ef305d1be206f7bc8736f859eeb56006534210
Javier Carey <javier3@gmail.com>
2019-02-07 14:37:12 -0800

    Added statsTable to coachPage

--- client/components/coachPage.js
+++ client/components/coachPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Map from './map'
-
+import StatsTable from './statsTable'
 
 export default function coachPage(props) {
     const runnerId = 2
     return (
       <React-fragment>
         <Map />
+        <StatsTable />
       </React-fragment>
     )
 
