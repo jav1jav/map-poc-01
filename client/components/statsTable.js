61ab7ca27d8a14b1e22e7dbc3456819d20ef8d97
Javier Carey <javier3@gmail.com>
2019-02-14 16:47:55 -0800

    Formatting the Stats table, converting time string to minutes:seconds

--- client/components/statsTable.js
+++ client/components/statsTable.js
@@ -2,16 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-const StatsTable = ({stats, lastStat}) => {
-  return stats.length > 0 ? (
-    <div>
-      <div>time:{lastStat.time}</div>
-      <div>hr:{lastStat.hr}</div>
-      <div>cad:{lastStat.cad}</div>
+const convertMilliseconds = ms => {
+  const totalSeconds = Math.floor(ms / 1000)
+  const seconds = totalSeconds % 60
+  const minutes = Math.floor(totalSeconds / 60)
+  const addZero = num => (num < 10 ? '0' + num : num)
+  return addZero(minutes) + ':' + addZero(seconds)
+}
+
+const StatsTable = props => {
+  const defaultStat = {time: 0, hr: 0, cad: 0}
+
+  const firstStatTime = props.lastStat.hr ? new Date(props.stats[0].time) : 0
+
+  const lastStat = props.lastStat.hr ? props.lastStat : defaultStat
+
+  const displayTime = props.lastStat.hr
+    ? convertMilliseconds(new Date(lastStat.time) - firstStatTime)
+    : '00:00'
+
+  return (
+    <div className="statsTable statsFormat">
+      <div className="statsColumn statsFormat">
+        <div className="statHeader statsFormat">time:</div>
+        <div className="stat statsFormat">{displayTime}</div>
+      </div>
+      <div className="statsColumn statsFormat">
+        <div className="statHeader statsFormat">hr:</div>
+        <div className="stat statsFormat">{lastStat.hr}</div>
+      </div>
+      <div className="statsColumn statsFormat">
+        <div className="statHeader statsFormat">cad:</div>
+        <div className="stat statsFormat">{lastStat.cad}</div>
+      </div>
     </div>
   )
-    :
-    <div>statsTable loading</div>
 }
 
 /**
