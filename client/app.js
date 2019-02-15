f3345cc96f5723b5b40a0caff23b45074fed4c73
Javier Carey <javier3@gmail.com>
2019-02-15 11:22:08 -0800

    Cleaned the garbage I'm not using out of app.js

--- client/app.js
+++ client/app.js
@@ -1,24 +1,5 @@
 import React from 'react'
-
-import {Navbar} from './components'
 import Routes from './routes'
-
-// const App = () => {
-
-//   // return (
-//   //   <div>
-
-//   //     {/* <Navbar /> */}
-
-//   //     {/* <div><a href="https://www.linkedin.com/hp/javiercarey">Developer: Javier Carey</a></div> */}
-//   //     <Routes />
-//   //   </div>
-//   // )
-
-
-
-// }
-
 
 const App = () => <Routes />
 
