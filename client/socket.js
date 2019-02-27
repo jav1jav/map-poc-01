a097e8905d9d387683468a2d164f623870e16119
Javier Carey <javier3@gmail.com>
2019-02-26 16:41:43 -0800

    BUGFIX: noticed that even when not logged in, browser would get a sockets connection. Messed with it for a long time and it looks like sockets.js gets 'processed' or 'loaded' or 'run' no matter what you do (even components I'm not using like navbar.js get processed), so in order to keep not-logged-in users from getting a socket connection I socket.disconnect immediately after creating it in sockets.js, and then user-home (which loads after login) will make the connection appropriately.

--- client/socket.js
+++ client/socket.js
@@ -3,7 +3,16 @@ import { broadcaster, convertStatsArrayToObj } from './components/runnerMapUtils
 import store from './store'
 import { gotStat } from './store/stat'
 
-const socket = io.connect(window.location.origin)
+//This makes me a little sad, to create the socket and then disconnect it
+//right away, but I'm missing something here. If I don't do this, then anyone
+//coming to the site automatically gets a socket connection even if they haven't
+//logged in. I set it so the socket isn't even imported by a component until user
+//has already logged in, but even so, connection gets made.
+//Disconnecting the socket after connecting, however, creates a socket that can
+//be managed by user-home.js (ie. connected/disconnected), but doesn't let anyone
+//not logged in get connected.
+const socket = io.connect()
+socket.io.disconnect()
 
 export const socketDisconnect = () => {
   socket.io.disconnect()
@@ -12,7 +21,6 @@ export const socketDisconnect = () => {
 export const socketReconnect = () => {
   socket.connect(window.location.origin, {'forceNew':true })
 }
-
 
 socket.on('connect', () => {
   console.log('Connected!')
